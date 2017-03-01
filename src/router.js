0a78d94bf0c034c48b7a1c0e588ac2e0b00bd0ed
Rafferty97 <alex@flashbang.io>
2017-03-01 21:26:20 +0800

    Tracking codes

--- src/router.js
+++ src/router.js
@@ -56,6 +56,13 @@ export default (props) => {
   return (
     <html>
       <head>
+        { /* <!-- Google Tag Manager --> */ }
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TD3SK9D');`}</script>
+        { /* <!-- End Google Tag Manager --> */ }
         <link rel="icon" type="image/png" sizes="32x32" href={require('./views/theme/favicon-32x32.png')} />
         <link rel="icon" type="image/png" sizes="16x16" href={require('./views/theme/favicon-32x32.png')} />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
@@ -63,6 +70,10 @@ export default (props) => {
         <title>{title}</title>
       </head>
       <body className={meta.bodyClass}>
+        { /* <!-- Google Tag Manager (noscript) --> */ }
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD3SK9D"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        { /* <!-- End Google Tag Manager (noscript) --> */ }
         <View {...viewProps} />
       </body>
     </html>
